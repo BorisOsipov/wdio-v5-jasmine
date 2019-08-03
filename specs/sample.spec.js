89e7ba124a88addf2479635b6a5ae30794f858f3
Boris Osipov <osipov.boris@gmail.com>
2019-08-03 16:28:58 +0300

    Some test stuff

--- specs/sample.spec.js
+++ specs/sample.spec.js
@@ -1,6 +1,19 @@
 describe('describe', function() {
-  it('it', function () {
-    browser.url('https://github.com/borisosipov');
-    expect($('.p-name').getText()).toContain('Boris Osipov')
-  });
+  Array(1).fill(5).forEach(function (val, idx)  {
+    it(`Passed ${idx}`, function () {
+      browser.url('https://github.com/borisosipov');
+      expect($('.p-name').getText()).toContain('Boris Osipov')
+    });
+
+    it(`Failed ${idx} @test @build`, function () {
+      browser.url('https://github.com/borisosipov');
+      expect($('.p-name').getText()).toContain('Boris2 Osipov')
+    });
+
+    xit(`Skipped ${idx} @some @issue`, function () {
+      browser.url('https://github.com/borisosipov');
+      expect($('.p-name').getText()).toContain('Boris2 Osipov')
+    });
+
+  })
 });
--- wdio.conf.js
+++ wdio.conf.js
@@ -1,5 +1,43 @@
+const reportportal = require('wdio-reportportal-reporter');
+const RpService = require("wdio-reportportal-service");
+const path = require("path");
+const fs = require("fs");
+
+const conf = {
+    reportPortalClientConfig: {
+        endpoint: 'http://35.159.46.231:8080/api/v1',
+        launch: 'osdsfsdff',
+        project: 'borisosipov_personal',
+        description: "Launch description text",
+        mode: 'DEFAULT',
+        debug: false,
+        tags: ["tags", "for", "launch"],
+    },
+    reportSeleniumCommands: false,
+    autoAttachScreenshots: false,
+    seleniumCommandsLogLevel: 'info',
+    screenshotsLogLevel: 'info',
+    parseTagsFromTestTitle: true,
+};
+
+// const conf = {
+//     reportPortalClientConfig: {
+//         endpoint: 'https://web.demo.reportportal.io/api/v1',
+//         launch: 'osdsfsdff',
+//         project: 'borisosipov_personal',
+//         description: "Launch description text",
+//         debug: false
+//     },
+//     reportSeleniumCommands: true,
+//     autoAttachScreenshots: false,
+//     seleniumCommandsLogLevel: 'debug',
+//     screenshotsLogLevel: 'info',
+//     parseTagsFromTestTitle: false,
+// };
 
 exports.config = {
+    debug: true,
+    execArgv: ["--inspect=127.0.0.1:5859"],
     specs: [
         './specs/**/*.js'
     ],
@@ -10,22 +48,16 @@ exports.config = {
       {
         maxInstances: 1,
         browserName: 'chrome',
-        'goog:chromeOptions': {
-          // to run chrome headless the following flags are required
-          // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
-          // args: ['--headless', '--disable-gpu'],
-        }
       },
 
   ],
-    logLevel: 'silent',   // Level of logging verbosity: silent | verbose | command | data | result | error
+    logLevel: 'error',   // Level of logging verbosity: silent | verbose | command | data | result | error
     deprecationWarnings: true,
     bail: 0,
     baseUrl: 'http://localhost',
     waitforTimeout: 10000,
     connectionRetryTimeout: 90000,
     connectionRetryCount: 3,
-    services: [],
     framework: 'jasmine',
     jasmineNodeOpts: {
         // Jasmine default timeout
@@ -38,22 +70,19 @@ exports.config = {
             // do something
         }
     },
-    reporters: [
-        'spec',
-        ['allure', {
-            disableWebdriverStepsReporting: true,
-            disableWebdriverScreenshotsReporting: false,
-          }
-        ],
-    ],
+    services: [[RpService, {}]],
+    reporters: ['spec', [reportportal, conf]],
 
     beforeSession: function (config, capabilities, specs) {
       require('@babel/register');
     },
 
     afterTest: function (test) {
-      if (test.error !== undefined) {
-        browser.takeScreenshot();
-      }
+        if (test.passed === false) {
+            const filename = "screnshot.png";
+            const outputFile = path.join(__dirname, filename);
+            browser.saveScreenshot(outputFile);
+            reportportal.sendFileToTest(test, 'info', filename, fs.readFileSync(outputFile));
+        }
     },
 }
